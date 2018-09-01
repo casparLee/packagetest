875450ec1234db031676959aa5db02b04071f230
casparlee <381244953@qq.com>
2018-09-01 10:30:07 +0800

    up demo
    updemo

---
+++ demoController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace App\Http\Controllers\Sales;
+
+use App\Http\Controllers\Common\BaseController;
+use App\Http\Controllers\Controller;
+use App\Models\Good;
+
+use App\Models\Tuan;
+use App\Models\TuanUser;
+use App\Models\SalesOrder;
+
+
+use DB;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Http\Request;
+use Caspar\Packagetest\Facades\Packagetest;
+
+class TuanController extends Controller
+{
+    public function kjdo($kid)
+    {
+        //帮忙砍价
+        $k_uid = 100;//砍价着uid
+
+        return Packagetest::goBargain($kid, $k_uid);
+    }
+
+    public function kjinfo($id)
+    {
+
+          $item=Packagetest::bargaindetail($id);
+
+          $goods=$item['goods'];
+          $item=$item['kjitem'][0];
+
+        return view('kanjiainfo', compact( 'goods','item'));
+    }
+
+    public function kjlist($uid)
+    {
+
+          $posts = Packagetest::listBargain($uid);
+
+        return view('kanjialist', compact( 'posts'));
+    }
+
+    public function huodonglist(){
+        $uid=123;
+        $res=Packagetest::huodonglist($uid);
+        $tuanlist=$res['tuan'];
+        $kanjialist=$res['kanjia'];
+        return view('huodonglist', compact( 'tuanlist','kanjialist'));
+    }
+
+    public function kjaddview(){
+        $uid=123;
+        $goods=Packagetest::goods($uid);
+
+        return view('tuanadd', compact( 'goods'));
+    }
+
+    /*新增砍价活动*/
+    public function kjadd(Request $request)
+    {
+        $userID=123;
+        if ($request->isMethod('post')) {
+            $goods_name=Packagetest::godoscloum($request->product_id, ['name']);
+            $data = [];
+            $data['user_id'] = empty($request->user_id)?$userID:$request->user_id;
+            $data['product_id'] = $request->product_id;
+            $data['product_name'] = $goods_name['title'];//商品名称
+            $data['activity_money'] = $request->activity_money; //活动价
+            $data['join_count'] = $request->join_count; //参与人数
+            $data['product_desc'] = $request->product_desc;//活动商品描述
+            $data['min_price'] = $request->min_price; //最小价格
+            $data['max_price'] = $request->max_price; //最大价格
+            $data['id'] = isset($request->bargain_id) ? $request->bargain_id : null; //最大价格
+            return Packagetest::addBargainAction($data);
+        }
+       return 0;
+    }
+
+    public function kjedit(Request $request)
+    {
+        /*测试demo 调用砍价活动控制器*/
+        $data = [];
+        $data['bargain_id'] = $request->bargain_id;//业务ID
+        $data['user_id'] = $request->user_id;
+        $data['product_id'] = $request->product_id;
+        $data['product_name'] = $request->product_name;//商品名称
+        $data['activity_money'] = $request->activity_money; //活动价
+        $data['join_count'] = $request->join_count; //参与人数
+        $data['product_desc'] = $request->product_desc;//活动商品描述
+        $data['min_price'] = $request->min_price; //最小价格
+        $data['max_price'] = $request->max_price; //最大价格
+        return Packagetest::addBargainAction($data);
+    }
+
+    public function tuandel($id)
+    {
+       return Packagetest::actityDEL(1, $id,0);
+    }
+    public function tuanup($id)
+    {
+        return  Packagetest::actityDEL(1, $id,1);
+    }
+    public function kanjiadel($id)
+    {
+        return  Packagetest::actityDEL(3, $id,0);
+    }
+    public function kanjiaup($id)
+    {
+        return   Packagetest::actityDEL(3, $id,1);
+    }
+    public function test(Request $request)
+    {
+
+        $a = Packagetest::goods_rtn(3, 13);
+        $goodsinfo = $a['goodsinfo'];
+        $tuan = $a['tuan'];
+        $user = $a['user'];
+
+        return view('tuan', compact('goodsinfo', 'tuan', 'user'));
+    }
+
+    /*
+     * 获取当前团购商品
+     * 当传了属性时，按属性值计算，没传时按第一个计算
+     */
+    public function goodsInfo($tid = '', $gid = '')
+    {
+        $resData = Packagetest::goods_rtn($tid, $gid);
+        $tuan = $resData['tuan'];
+        $user = $resData['user'];
+
+        // 查出来商品信息，关联查询出对应属性及属性名称
+        $goodsinfo = $resData['goodsinfo'];
+        //dd($goodsinfo);
+
+        return view('tuan', compact('goodsinfo', 'tuan', 'user'));
+    }
+
+
+    public function addTuan(Request $request)
+    {
+        $goods_name=Packagetest::godoscloum($request->good_id, ['name']);
+
+        $data['good_id'] = $request->good_id;
+        $data['good_title'] = $goods_name['title'];
+        $data['title'] = $request->title;
+        $data['tuan_num'] = $request->tuan_num;
+        $data['buy_num'] = $request->buy_num;
+        $data['store'] = $request->store;
+        $data['price'] = $request->price;
+        $data['starttime'] = strtotime($request->starttime);
+        $data['endtime'] = strtotime($request->endtime);
+        $data['status'] = empty($request->status)?0:(int)$request->status;
+
+        return Packagetest::addTuan($data);
+    }
+
+    public function postTuan(Request $request)
+    {
+
+        $data['sid'] = $request->sid;
+        $data['gid'] = $request->gid;
+        $data['spec_key'] = $request->spec_key;
+        $data['num'] = $request->num;
+        $data['uid'] = $request->uid;
+        $data['gp'] = $request->gp;
+        $datalist= Packagetest::postTuan($data);
+        try {
+            // 重新计算价格
+            $price = $old_price = $datalist['tuan']->price;
+            $prices = $price * $data['num'];
+
+            // 创建订单
+            $order = [ 'user_id' => $data['uid'], 'yhq_id' => 0, 'yh_price' => 0,
+                'old_prices' => $old_price, 'total_prices' => $prices, 'create_ip' => '127.0.0.1',
+                'address_id' => 0, 'ziti' => 0, 'area' => '', 'mark' => '', 'prom_type' => 2,
+                'sales_id' => $data['sid'], 'display' => 0];
+            $order = SalesOrder::create($order);
+
+            DB::commit();
+           $this->ajaxReturn('1', $order);
+
+        } catch (\Throwable $e) {
+            // 出错回滚
+            DB::rollBack();
+            $this->ajaxReturn('0', $e->getMessage());
+        }
+
+    }
+
+    public function backTuan($oid)
+    {
+        /*
+         * 1、是否开团，开团商品不允许
+         * 2、谁退 user_id
+         * 3、退哪 tuan_user状态取消，促销订单表 status取消 pay_status 回款
+         * 4、记录财务退款表 退款接口
+         * */
+
+        $order = SalesOrder::where('id', $oid)->first();
+
+        $tuanInfo = Tuan::where('tid', $order->sales_id)->first();
+        if (time() > $tuanInfo->endtime) {
+            $this->ajaxReturn('0', '已经开团，不允许退团！');
+        }
+        /*修改订单状态*/
+        $resUser = TuanUser::where('t_id', $order->sales_id)
+            ->where('user_id', $order->user_id)
+            ->update(['status' => 2]);
+
+        $resOrder = SalesOrder::where('id', $oid)
+            ->update(['status' => 2]);
+        $this->ajaxReturn('1', '退款成功！');
+    }
+
+
+}
+
